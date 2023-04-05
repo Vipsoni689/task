8565b1f52a4cb485d09a99218f5b0b038b71976e
vipul soni <109866709+Vipsoni689@users.noreply.github.com>
2023-04-05 23:25:59 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,19 @@
+document.getElementById("toggle").onclick =()=>{
+    var main_slide=document.getElementById("main_slide")
+
+    main_slide.classList.add("active")
+    
+}
+
+function cut1(){
+    var main_slide=document.getElementById("main_slide")
+    main_slide.classList.remove("active")
+}
+
+
+function form1(){
+    document.getElementById("form").classList.add("active")
+}
+function cut2(){
+    document.getElementById("form").classList.remove("active")
+}
